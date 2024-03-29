31793ab478009f686121141b8283a633310c6305
Azi0830 <turakulov.t.a@gmail.com>
2024-03-29 14:44:23 +0900

    await

--- index.js
+++ index.js
@@ -22,11 +22,11 @@
 
 //////////////////////////////////////////////////
 
-var url = "https://cute-jade-binturong-boot.cyclic.app/api";
+var url = "https://cute-jade-binturong-boot.cyclic.app/";
 
 var get_flovers = async (search) => {
   var response = await fetch(
-    `${url}/flower/category?access_token=64bebc1e2c6d3f056a8c85b7`
+    `${url}api/flower/category?access_token=64bebc1e2c6d3f056a8c85b7`
   );
 
   var data = await response.json();
@@ -35,14 +35,13 @@ var get_flovers = async (search) => {
     return value.title.includes(search);
   });
 
-  if (found_data.lenght === 0) {
+  if (found_data.length === 0) {
     console.log("gul topilmadi");
   } else {
     for (let i = 0; i < found_data.length; i++) {
       console.log(found_data[i].title);
     }
-    //   console.log(found_data);
   }
 };
 
-get_flovers("Plants");
+get_flovers("Plantsss", ["title"]);
