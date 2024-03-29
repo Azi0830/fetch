23473e34b252ddab5278f044304ffcf01c2125d4
Azi0830 <turakulov.t.a@gmail.com>
2024-03-30 05:48:42 +0900

    sing-up

--- index.js
+++ index.js
@@ -22,26 +22,76 @@
 
 //////////////////////////////////////////////////
 
-var url = "https://cute-jade-binturong-boot.cyclic.app/";
+// var url = "https://cute-jade-binturong-boot.cyclic.app/";
 
-var get_flovers = async (search) => {
+// var get_flovers = async (search) => {
+//   var response = await fetch(
+//     `${url}api/flower/category?access_token=64bebc1e2c6d3f056a8c85b7`
+//   );
+
+//   var data = await response.json();
+
+//   var found_data = data.data.filter((value) => {
+//     return value.title.includes(search);
+//   });
+
+//   if (found_data.length === 0) {
+//     console.log("gul topilmadi");
+//   } else {
+//     for (let i = 0; i < found_data.length; i++) {
+//       console.log(found_data[i].title);
+//     }
+//   }
+// };
+
+// get_flovers("Plantsss", ["title"]);
+
+/////////
+
+// var data = [
+//   // { name: "azim", surname: "turakulov", age: "25" },
+//   // { name: "sardor", surname: "suyunov", age: "27" },
+//   // { name: "husan", surname: "muhiddinov", age: "17" },
+//   ["a", [[[[[["b"]]]]]], "c"],
+//   ["k", [[[[[["h"]]]]]], "s"],
+//   ["w", [[[[[["j"]]]]]], "d"],
+// ];
+
+// data.map(([bir, ikki, uch], index, array) => {
+//   console.log(ikki.flat(Infinity)[0]);
+// });
+
+// var { name, surname, age } = data;
+
+/////
+
+var url = "https://cute-jade-binturong-boot.cyclic.app/api";
+var onAuth = async ({ name, surname, password, email }) => {
   var response = await fetch(
-    `${url}api/flower/category?access_token=64bebc1e2c6d3f056a8c85b7`
-  );
-
-  var data = await response.json();
-
-  var found_data = data.data.filter((value) => {
-    return value.title.includes(search);
-  });
-
-  if (found_data.length === 0) {
-    console.log("gul topilmadi");
-  } else {
-    for (let i = 0; i < found_data.length; i++) {
-      console.log(found_data[i].title);
+    `${url}/user/sign-up?access_token=64bebc1e2c6d3f056a8c85b7`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        surname: surname,
+        password: password,
+        email: email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-  }
+  );
+  var data = await response.json();
+  console.log(data);
 };
 
-get_flovers("Plantsss", ["title"]);
+console.log(
+  onAuth({
+    name: "azimjon",
+    surname: "turakulov",
+    password: "123456789",
+    email: "azimjonturkulob1223m@gmail.com",
+  })
+);
+/////
